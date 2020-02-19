22585418a503d48c86dcf8b8cdfdd68dc03baddd
Dilendra Prasad Pant <dilendraprasadpant@Dilendras-MacBook-Pro.local>
2020-02-20 01:25:42 +0530

    Calling Movie API call

--- movie-info-service/src/main/java/com/dilendra/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/dilendra/movieinfoservice/resources/MovieResource.java
@@ -1,17 +1,29 @@
 package com.dilendra.movieinfoservice.resources;
 
 import com.dilendra.movieinfoservice.models.Movie;
+import com.dilendra.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource
 {
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{moviId}")
     public Movie getMovie(@PathVariable("moviId") String moviId){
 
-        return new Movie(moviId, "Mov01");
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + moviId + "?api_key=" + apiKey, MovieSummary.class);
+
+        return new Movie(moviId, movieSummary.getTitle());
     }
 }
